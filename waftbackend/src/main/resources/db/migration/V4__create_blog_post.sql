8ff8ffdd4686cbbe0571bc1a02e6ca8e7bfea298
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-07-15 12:01:56 +0545

    script for create_blog_post completed

--- waftbackend/src/main/resources/db/migration/V4__create_blog_post.sql
+++ waftbackend/src/main/resources/db/migration/V4__create_blog_post.sql
@@ -1,5 +1,20 @@
 CREATE TABLE blog_post(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(500) NOT NULL ,
+    slug VARCHAR(200) NOT NULL ,
+    title VARCHAR(200) NOT NULL ,
+    category VARCHAR(100),
+    cover_image_url VARCHAR(500),
+    excerpt VARCHAR(300),
+    content LONGTEXT,
+    featured BOOLEAN NOT NULL DEFAULT FALSE,
+    is_published BOOLEAN NOT NULL DEFAULT TRUE,
+    author_id BIGINT NOT NULL ,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT unique_blog_post_slug UNIQUE (slug),
+    CONSTRAINT fk_blog_creator FOREIGN KEY (author_id) REFERENCES waft_user(id) ON DELETE RESTRICT
+
+
 
 )
