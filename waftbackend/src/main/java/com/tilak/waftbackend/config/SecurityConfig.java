38d4e290bba4ad869308f16bf1d7ec524a090676
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-07-16 15:20:14 +0545

    spring security enabled for password hashing

---
+++ waftbackend/src/main/java/com/tilak/waftbackend/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.tilak.waftbackend.config;
+
+import jakarta.servlet.http.HttpServlet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain config(HttpSecurity http) throws Exception
+    {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(request-> request.anyRequest().permitAll());
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+}
--- waftbackend/src/main/java/com/tilak/waftbackend/service/WaftUserService.java
+++ waftbackend/src/main/java/com/tilak/waftbackend/service/WaftUserService.java
@@ -1,7 +1,20 @@
 package com.tilak.waftbackend.service;
 
+import com.tilak.waftbackend.repository.WaftUserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class WaftUserService {
+
+    private final WaftUserRepo waftUserRepo;
+    private final PasswordEncoder passwordEncoder;
+
+
+
+
+
+
 }
