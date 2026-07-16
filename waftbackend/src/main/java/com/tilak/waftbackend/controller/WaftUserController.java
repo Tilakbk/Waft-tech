5222ed06028dc051cd89061970a508373a18926f
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-07-16 15:22:25 +0545

    WaftUserController added

---
+++ waftbackend/src/main/java/com/tilak/waftbackend/controller/WaftUserController.java
@@ -0,0 +1,12 @@
+package com.tilak.waftbackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class WaftUserController {
+
+
+
+}
