908b0d02e8c938dae22d1ad879cc1e263e05970f
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-07-22 12:55:45 +0545

    migration file added for bootstrapping initial admin

---
+++ waftbackend/src/main/resources/db/migration/V11__add_bootstrap_admin.sql
@@ -0,0 +1,9 @@
+INSERT INTO waft_user (name, email, password_hash, role, is_active, created_by_id)
+VALUES (
+           'Waft Admin',
+           'admin@wafttech.io',
+           '$2a$12$PABPTrIdSrcl5DxbCtcxNOsnsFF0/f/M2VCly2PQKLUPS6gfIOUzC',
+           'ADMIN',
+           TRUE,
+           NULL
+       );
