2410a7ef14f5ecbeccec9ab35a9a7a9a2297caa7
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-07-27 15:43:27 +0545

    Project request dto completed

--- waftbackend/src/main/java/com/tilak/waftbackend/dto/request/ProjectRequestDto.java
+++ waftbackend/src/main/java/com/tilak/waftbackend/dto/request/ProjectRequestDto.java
@@ -1,4 +1,39 @@
 package com.tilak.waftbackend.dto.request;
 
+import com.tilak.waftbackend.record.SolutionItem;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProjectRequestDto {
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    private List<String> tags;
+
+    private String thumbnailUrl;
+
+    private String heroImageUrl;
+
+    private LocalDate date;
+
+    private String brief;
+
+    private String problemStatement;
+
+    private List<SolutionItem> solutions;
+
+    private List<SolutionItem> results;
+
+    private String finalThought;
 }
