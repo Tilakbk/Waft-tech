b6b036b74394445dc1f69c3541fbafe4aeb22928
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-07-27 15:04:29 +0545

    ProjectImageRepo added

---
+++ waftbackend/src/main/java/com/tilak/waftbackend/repository/ProjectImageRepo.java
@@ -0,0 +1,9 @@
+package com.tilak.waftbackend.repository;
+
+import com.tilak.waftbackend.model.ProjectImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectImageRepo extends JpaRepository<ProjectImage,Long> {
+}
