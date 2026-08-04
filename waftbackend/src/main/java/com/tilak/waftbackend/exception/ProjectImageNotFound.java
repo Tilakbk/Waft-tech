2c6b2c0183c407c37966f96ec474d1c118b2e260
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-08-04 10:33:05 +0545

    project image not found exp added

---
+++ waftbackend/src/main/java/com/tilak/waftbackend/exception/ProjectImageNotFound.java
@@ -0,0 +1,7 @@
+package com.tilak.waftbackend.exception;
+
+public class ProjectImageNotFound extends RuntimeException {
+    public ProjectImageNotFound(String message) {
+        super(message);
+    }
+}
