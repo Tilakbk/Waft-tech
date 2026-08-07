6bf70999a80954ba6c61165e10375ee8ca961a72
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2026-08-07 12:44:29 +0545

    service for addBlogPost added

--- waftbackend/src/main/java/com/tilak/waftbackend/service/BlogPostService.java
+++ waftbackend/src/main/java/com/tilak/waftbackend/service/BlogPostService.java
@@ -3,15 +3,33 @@ package com.tilak.waftbackend.service;
 import com.tilak.waftbackend.dto.request.BlogPostRequestDto;
 import com.tilak.waftbackend.dto.request.BlogPostUpdateRequestDto;
 import com.tilak.waftbackend.dto.response.BlogPostResponseDto;
+import com.tilak.waftbackend.mapper.Mapper;
+import com.tilak.waftbackend.model.BlogPost;
 import com.tilak.waftbackend.model.WaftUser;
-import jakarta.validation.Valid;
+import com.tilak.waftbackend.repository.BlogPostRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BlogPostService {
-    public BlogPostResponseDto addBlogPost(@Valid BlogPostRequestDto requestDto, WaftUser waftUser) {
+
+    private final BlogPostRepo repo;
+    public BlogPostResponseDto addBlogPost( BlogPostRequestDto requestDto, WaftUser waftUser) {
+
+        BlogPost blogPost= Mapper.toBlogPost(requestDto);
+        blogPost.setAuthor(waftUser);
+        blogPost.setSlug(generateSlug(requestDto.getTitle()));
+        return Mapper.toBlogPostResponseDto(repo.save(blogPost));
+    }
+
+    private String generateSlug(String title) {
+        return title.toLowerCase()
+                .trim()
+                .replaceAll("[^a-z0-9\\s-]", "")
+                .replaceAll("\\s+", "-");
     }
 
     public Page<BlogPostResponseDto> getAllBlogPostsAdmin(Pageable pageable, WaftUser waftUser) {
